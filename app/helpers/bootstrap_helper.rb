454e55a070c9bcc57d698b48d883b7597d7af2a8
Mufudzi Masaire <mufudzimasaire@gmail.com>
2015-04-16 22:44:11 +0100

    adding bootstrap alert helper

---
+++ app/helpers/bootstrap_helper.rb
@@ -0,0 +1,13 @@
+module BootstrapHelper
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = :success if type.eql?("notice")
+      type = :danger if type.eql?("alert")
+      text = content_tag(:div, link_to("x", "#", class: "close", "data-dismiss" => "alert") + message, class: "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+end
+
