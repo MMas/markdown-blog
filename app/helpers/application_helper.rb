6780e25f4d180354775c71ef25b85206da7609c9
Mufudzi Masaire <mufudzimasaire@gmail.com>
2015-04-16 22:10:37 +0100

    adding site title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_site_title(page_title)
+    site_name = "Markdown Blog"
+    if page_title.present?
+      "#{page_title} - #{site_name}"
+    else
+      "#{site_name}"
+    end
+  end
+
 end
