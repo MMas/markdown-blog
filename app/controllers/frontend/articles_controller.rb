8b1e805fdbcddec9fb7ed1cd19193c9e17f6ca7e
Mufudzi Masaire <mufudzimasaire@gmail.com>
2015-04-17 22:45:15 +0100

    adding frontend article controller and helper file

---
+++ app/controllers/frontend/articles_controller.rb
@@ -0,0 +1,33 @@
+class Frontend::ArticlesController < Frontend::ApplicationController
+
+  def index
+    @articles = Article.all.page(params[:page]).per(5)
+  end
+
+  def show
+    @article = Article.friendly.find(params[:id])
+    @comment = @article.comments.new
+    @comments = @article.comments.recent.limit(10).all
+  end
+
+  def new_comment
+    @article = Article.friendly.find(params[:id])
+    @comment = @article.comments.new
+    @comment.title = params[:comment][:title]
+    @comment.comment = params[:comment][:comment]
+
+    if @comment.save
+      redirect_to [:frontend, @article], notice: "Successfully posted comment."
+    else
+      redirect_to [:frontend, @article], alert: "Could not post comment."
+    end
+  end
+
+
+  private
+
+    def comments_params
+      params.require(:comment).permit(:commentable)
+    end
+
+end
