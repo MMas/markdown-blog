56a8699ee423e7abc53bb5db79649e19020d01bc
Mufudzi Masaire <mufudzimasaire@gmail.com>
2015-04-17 06:41:50 +0100

    adding acts as commentable to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :name, use: :slugged
+  
+  acts_as_commentable
 
   validates :name, presence: true, uniqueness: true
   validates :content, presence: true
