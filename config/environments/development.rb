bede814a2426869f0a945d393dfd24d2f3962a82
Mufudzi Masaire <mufudzimasaire@gmail.com>
2015-04-18 08:25:38 +0100

    adding action mailer configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # to be appraised of mailing errors
+  config.action_mailer.raise_delivery_errors = true
+  # to deliver to the browser instead of email
+  config.action_mailer.delivery_method = :letter_opener
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
 end
