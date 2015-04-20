27c918b51b780940be22fffd79bc5d86c852f612
Mufudzi Masaire <mufudzimasaire@gmail.com>
2015-04-20 21:48:09 +0100

    adding spec test for unsubscribing

--- spec/features/subscription_spec.rb
+++ spec/features/subscription_spec.rb
@@ -10,11 +10,19 @@ feature 'subscription' do
     expect(page).to have_content('subscribed')
   end
 
+  scenario 'is unsubscribed' do
+    unsubscribe
+    expect(page).to have_content('unsubscribed')
+  end
 
   def create_subscription
     visit frontend_root_path
     find('#email').set('test@test.com')
     click_button 'Subscribe'
+  end
+
+  def unsubscribe
+    visit unsubscribe_frontend_subscriptions_path(Subscriber.first)
   end
 
 end
